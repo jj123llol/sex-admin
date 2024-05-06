b0058e0b511e03bb3449025cc4ee8a2375ff2c21
jj123llol <115508192+jj123llol@users.noreply.github.com>
2024-05-05 20:50:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,6 @@
 document.getElementById('Discord').onclick = function() {
     window.open('https://discord.gg/gyYTywnpug', '_blank');
 }
-document.getElementById('githSource').onclick = function() {
-    window.open('https://github.com/abhisheknaiiduh/revs-admin/blob/main/universal%20admin', '_blank')
-}
 document.getElementById('Youtube').onclick = function() {
     window.open('https://youtube.com/@normalexploiter123', '_blank');
 }
